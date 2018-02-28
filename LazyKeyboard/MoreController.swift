65b3b2100a3d7fcd3e0f96ddb4ac1cbe57d2875f
CepheusSun <cd_sunyang@163.com>
2018-02-28 10:19:25 +0800

    UI 骨架

---
+++ LazyKeyboard/MoreController.swift
@@ -0,0 +1,30 @@
+//
+//  MoreController.swift
+//  LazyKeyboard
+//
+//  Created by sunny on 2018/2/28.
+//  Copyright © 2018年 CepheusSun. All rights reserved.
+//
+
+import UIKit
+
+class MoreController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+
+}
+
+
+extension MoreController: UITableViewDataSource {
+    
+    
+}
---
+++ LazyKeyboard/MoreEntity.swift
@@ -0,0 +1,20 @@
+//
+//  MoreEntity.swift
+//  LazyKeyboard
+//
+//  Created by sunny on 2018/2/28.
+//  Copyright © 2018年 CepheusSun. All rights reserved.
+//
+
+import Foundation
+
+struct MoreSection {
+    var header: String?
+    var footer: String?
+    
+    var items: [MoreItem] = []
+}
+
+struct MoreItem {
+    var title: String = ""
+}
