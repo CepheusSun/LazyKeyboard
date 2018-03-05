aa463698d5ece5eb9239382f9e200fdc51b87f20
CepheusSun <cd_sunyang@163.com>
2018-03-05 17:21:59 +0800

    ok

--- LazyKeyboard/General/Cloud/Document.swift
+++ LazyKeyboard/General/Cloud/Document.swift
@@ -7,17 +7,18 @@
 //
 
 import UIKit
+//
+//class Document: UIDocument {
+//    // 文档数据
+//    var data: NSData?
+//    
+//    override func contents(forType typeName: String) throws -> Any {
+//        self.data = NSData()
+//        return self.data
+//    }
+//    
+//    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+//        self.data = contents
+//    }
+//}
 
-class Document: UIDocument {
-    // 文档数据
-    var data: NSData?
-    
-    override func contents(forType typeName: String) throws -> Any {
-        self.data = NSData()
-        return self.data
-    }
-    
-    override func load(fromContents contents: Any, ofType typeName: String?) throws {
-        self.data = contents
-    }
-}
